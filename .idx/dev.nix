5a51e447f308e6205c643b3957515d421fa1dbde
shin <shin@sforzando.co.jp>
2024-08-10 01:38:24 +0000

    feat: :+1: set ENV

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,7 +10,11 @@
     pkgs.uv
   ];
   # Sets environment variables in the workspace
-  env = { };
+  env = {
+    DJANGO_ALLOWED_HOSTS = "*";
+    DJANGO_DEBUG = "True";
+    DJANGO_SECRET_KEY = "django-insecure-+@6#6u791^+&s&nntn8mp1xbmc(wr)@8n68^o#7x-7xz9td9c*";
+  };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -22,9 +26,7 @@
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        rye-sync = ''
-          rye sync
-        '';
+        rye-sync = "rye sync";
         # Open editors for the following files by default, if they exist:
         default.openFiles = [ "README.md" "pyproject.toml" ];
       };
